41f28df9da3ef9a5467e2ace0303cb306de76d73
mathiasmramba001 <kennedymwokozi@gmail.com>
2022-10-14 17:51:26 -0700

    final

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point - output a string of arrays in a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
