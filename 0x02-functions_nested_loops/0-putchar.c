3b79e51c85c3a9a17f186794dff50832e9d34a81
mathiasmramba001 <kennedymwokozi@gmail.com>
2022-10-19 13:22:25 -0700

    nested q0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,10 @@
 */
 int main(void)
 {
-	_putchar("_"); 
-        _putchar("p"); 
+	void _putchar();
+
+	_putchar('_'); 
+        _putchar('p'); 
         _putchar('u'); 
         _putchar('t'); 
         _putchar('c'); 
