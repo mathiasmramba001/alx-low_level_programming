216e18a46658682848073fcd010026862aa951b6
mathiasmramba001 <kennedymwokozi@gmail.com>
2022-10-19 17:33:27 -0700

    second betty understanding

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10(void)- a program to print alphabets ten times
+*
+* return success value
+*/
+
+void print_alphabet_x10(void);
+{
+	int w;
+	int y;
+
+	for (w = 0; w < 10; x++)
+	{
+	for (y = 'a'; y < = 'z'; y++)
+		{
+		_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
