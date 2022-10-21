294eece768de48c44af3b82663a7aab72852d765
mathiasmramba001 <kennedymwokozi@gmail.com>
2022-10-21 12:51:32 -0700

    qn1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for characters that are digits
-* 
-* @c: character 
+* _isdigit - checks for characters that are digits
 *
+* @c: character*
 * Return: 0 or 1
 */
 int _isdigit(int c) /*calling of the fuctions*/
