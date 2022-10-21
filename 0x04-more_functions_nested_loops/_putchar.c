94d513f1fbc70177014a9756f00e03c7e6dda070
mathiasmramba001 <kennedymwokozi@gmail.com>
2022-10-21 12:28:03 -0700

    qn0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,10 +3,12 @@
 
 /**
 * _putchar -> this writes the character c to stdout
+*
 *@c: this takes the character to print
-* return: On success 1 else -1
+*
+* Return: On success 1 else -1
 */
 int _putchar(char c)
 	{
-	return (write(1, &c, -1));/*returning a value digit*/
+	return (write(1, &c, -1)); /*returning a value digit*/
 	}
