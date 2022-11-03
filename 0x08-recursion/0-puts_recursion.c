0103aad1aa532857a80cca6a093082dc38e10799
mathiasmramba001 <kennedymwokozi@gmail.com>
2022-11-03 06:21:08 -0700

    qn0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_puts_recursion -prints a string folled by a new line
+  *
+  *@s: this takes a pointer to the string
+  *
+  *returns: always void
+  */
+void _puts_recursion(char *s)
+{
+	if (s[0] =='\0')
+	{
+		_putchar('\n');
+	return;
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
